eec99aed2878822971c65bc965c79dd8875f2d6b
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-11-22 02:04:39 +0600

    scleton of chatapp

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv"
+// import authRoutes from "./routes/auth.route"
+import authRoutes from "./routes/auth.route.js";  
+import messageRoutes from "./routes/message.route.js";  
+dotenv.config();
+const app = express();
+
+// console.log(process.env.PORT);
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+
+app.listen(PORT, () => console.log("Server running on port:" +PORT));
