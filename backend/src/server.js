594547eb8f3cf6a13cb088280c705b40e204c6b7
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-11-22 12:54:30 +0600

    ready for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,12 +17,13 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
 //make deployment
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*",(_,res)=> {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    })
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
 }
+
 
 app.listen(PORT, () => console.log("Server running on port:" +PORT));
